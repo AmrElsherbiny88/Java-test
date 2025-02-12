0fa6ad9929306b255813c582c56364e01e0302b7
AmrElsherbiny <amoryelsherbiny@gmail.com>
2025-02-12 16:27:20 +0200

    variables

---
+++ variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args) {
+        int age = 30 ; 
+        System.out.println("I am " + age + " years old.");
+
+
+        double price = 10.5;
+        System.out.println("The price of the book is " + price + " dollars.");
+
+        
+        String  name = "John";
+        System.out.println("My name is " + name + ".");
+         
+
+        boolean isAdult = false;
+        System.out.println("Am I an adult? " + isAdult);
+
+        char grade = 'A';
+        System.out.println("My grade is " + grade);
+    }
+}
