222f269964abf84747da44103d24d920b3ca62da
AmrElsherbiny <amoryelsherbiny@gmail.com>
2025-02-14 07:50:17 +0200

    Create UserInputs.java

---
+++ UserInputs.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class UserInputs {
+    
+     public static void main(String[] args) {
+
+
+        // ************************* user input *************************
+
+        //  create a scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        //  message to the user
+          System.out.println("Enter your name: ");
+
+        //    get the user input
+          String name = scanner.nextLine();
+
+        //   print the user input
+          System.out.println("Hello, " + name + "!");
+
+          
+ 
+        System.out.println("Enter your age: ");
+
+        int age = scanner.nextInt();
+        
+        System.out.println("You are " + age + " years old.");
+
+        
+        //   close the scanner
+        scanner.close();
+
+        // ************************* user input *************************
+
+
+
+
+
+
+
+
+
+
+
+
+     }
+}
