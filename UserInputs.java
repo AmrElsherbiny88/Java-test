8b2054e39c5c4991d5b2a351d1a73849a18c4bf5
AmrElsherbiny <amoryelsherbiny@gmail.com>
2025-02-14 08:06:24 +0200

    Update UserInputs.java

--- UserInputs.java
+++ UserInputs.java
@@ -1,27 +1,24 @@
+
 import java.util.Scanner;
 
 public class UserInputs {
-    
-     public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
         // ************************* user input *************************
-
         //  create a scanner object
         Scanner scanner = new Scanner(System.in);
 
         //  message to the user
-          System.out.println("Enter your name: ");
+        System.out.println("Enter your name: ");
 
         //    get the user input
-          String name = scanner.nextLine();
+        String name = scanner.nextLine();
 
         //   print the user input
-          System.out.println("Hello, " + name + "!");
-
+        System.out.println("Hello, " + name + "!");
 
         // **************************
- 
         System.out.println("Enter your age: ");
 
         int age = scanner.nextInt();
@@ -29,47 +26,32 @@ public class UserInputs {
         System.out.println("You are " + age + " years old.");
 
         // **************************
-
         System.out.println("Enter your GPA: ");
 
         double gpa = scanner.nextDouble();
 
         System.out.println("Your GPA is " + gpa);
-        
 
-         // **************************
-         
-         System.out.println("Are you a student? (true/false)");
+        // **************************
+        System.out.println("Are you a student? (true/false)");
 
-         boolean isStudent = scanner.nextBoolean();
+        boolean isStudent = scanner.nextBoolean();
 
-
-         
-         if (isStudent) {
-          System.out.println("You are a student.");
-         }else {
-          System.out.println("You are not a student.");
-         }
-
-
-
-
+        if (isStudent) {
+            System.out.println("You are a student.");
+        } else {
+            System.out.println("You are not a student.");
+        }
 
         //   close the scanner
         scanner.close();
 
         // ************************* user input *************************
-
-
-
-
-
-
-
-
-
-
-
-
-     }
+        // another example 
+        // Scanner type = new Scanner(System.in);
+        // System.out.println("Enter your gender: ");
+        // String gender = type.nextLine();
+        // System.out.println("your gender is" + gender);
+        // type.close();
+    }
 }
