24d3cb5c2721b22c7e29233f40a6b8c5a8a95b55
AmrElsherbiny <amoryelsherbiny@gmail.com>
2025-02-14 08:14:36 +0200

    Create recAreaWithVariablesAndInputs.java

---
+++ recAreaWithVariablesAndInputs.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class recAreaWithVariablesAndInputs {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+              
+               System.out.println("Enter the length : ") ; 
+               double length = scanner.nextDouble();
+               scanner.nextLine();  
+
+               System.out.println("Enter the width : ") ;
+               double width = scanner.nextDouble();
+                scanner.nextLine();
+
+                double area = length * width;
+                
+                System.out.println("The area of the rectangle is " + area + " square units.");
+             
+         scanner.close();
+
+    }
+}
